100c442e0aa4c5a28e69d1f692466f839ca2f8f7
Natepei <125333413+Natepei@users.noreply.github.com>
2023-03-06 03:56:44 +0700

    Add files via upload

---
+++ 2.2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int T(int n) {
+	if (n == 0) {
+		return 1;
+	}
+	else {
+		return n * T(n - 1);
+	}
+}
+int main() {
+	cout << "10! = " << T(10) << endl;
+}
